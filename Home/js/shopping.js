8d45a40aa6bb8ed262eb097175d0b07d3a779462
Robert Philippe Gerong <philippe.gerong@gmail.com>
2025-01-12 15:55:11 +0800

    added closing div for app 2

--- Home/js/shopping.js
+++ Home/js/shopping.js
@@ -1,30 +1,30 @@
 const app3 = Vue.createApp({
-    data() {
-      return {
-        itemName: null,
-        itemNumber: null,
-        itemImportant: false,
-        shoppingList: [
-          { name: 'Tomatoes', number: 5, important: false, found: false },
-          { name: 'Bread', number: 1, important: false, found: false },
-          { name: 'Soap', number: 1, important: true, found: true },
-        ],
+  data() {
+    return {
+      itemName: null,
+      itemNumber: null,
+      itemImportant: false,
+      shoppingList: [
+        { name: 'Tomatoes', number: 5, important: false, found: false },
+        { name: 'Bread', number: 1, important: false, found: false },
+        { name: 'Soap', number: 1, important: true, found: true },
+      ],
+    };
+  },
+  methods: {
+    addItem() {
+      const newItem = {
+        name: this.itemName,
+        number: this.itemNumber,
+        important: this.itemImportant,
+        found: false,
       };
+      this.shoppingList.push(newItem);
+
+      this.itemName = null;
+      this.itemNumber = null;
+      this.itemImportant = false;
     },
-    methods: {
-      addItem() {
-        const newItem = {
-          name: this.itemName,
-          number: this.itemNumber,
-          important: this.itemImportant,
-          found: false,
-        };
-        this.shoppingList.push(newItem);
-  
-        this.itemName = null;
-        this.itemNumber = null;
-        this.itemImportant = false;
-      },
-    },
-  });
-  app3.mount('#app3');
+  },
+});
+app3.mount('#app3');
