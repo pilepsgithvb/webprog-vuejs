e7f4af474e154f4485fd5faee40caf3789d7c574
Robert Philippe Gerong <philippe.gerong@gmail.com>
2025-02-09 17:35:53 +0000

    fixed comment

--- vue-project/src/components/Comment.vue
+++ vue-project/src/components/Comment.vue
@@ -1,39 +1,11 @@
 <template>
-    <h1>Countries</h1>
-    <ul>
-      <li v-for="country in countries" :key="country.id">{{ country.name }}</li>
-    </ul>
-  </template>
-  
-  <script></script>
-  
-  <style>
-    #app > div {
-      border: dashed black 1px;
-      display: inline-block;
-      margin: 10px;
-      padding: 10px;
-      background-color: lightyellow;
-    }
-  </style>
+  <h1>Comments</h1>
+  <ul>
+    <li v-for="comment in comments" :key="comment.id">{{ comment.name }} {{ comment.comment }}</li>
+  </ul>
+</template>
 
-<script setup>
-import { ref, onMounted } from 'vue'
-import { supabase } from '../lib/supabaseClient'
-
-const countries = ref([])
-
-async function getCountries() {
-  const { data } = await supabase.from('countries').select()
-  countries.value = data
-}
-
-onMounted(() => {
-  getCountries()
-})
-
-</script>
-
+<script></script>
 
 <style>
   #app > div {
@@ -43,4 +15,32 @@ onMounted(() => {
     padding: 10px;
     background-color: lightyellow;
   }
+</style>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import { supabase } from '../lib/supabaseClient'
+
+const comments = ref([])
+
+async function getComments() {
+const { data } = await supabase.from('comments').select()
+comments.value = data
+}
+
+onMounted(() => {
+getComments()
+})
+
+</script>
+
+
+<style>
+#app > div {
+  border: dashed black 1px;
+  display: inline-block;
+  margin: 10px;
+  padding: 10px;
+  background-color: lightyellow;
+}
 </style>
